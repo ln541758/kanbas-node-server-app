7440c5b857ebca09f487dae83d11e5d45e00d0f1
003 <apple@003-MacBook-Air.local>
2024-07-27 14:24:08 +0800

    Mongo

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
-import express from 'express';
+import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import "dotenv/config";
+import UserRoutes from "./Users/routes.js";
 
 const app = express();
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 
-app.use(cors()); 
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json()); // do all work after this line
+
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentRoutes(app);
