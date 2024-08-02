d7c419f15970b88e901124c57708883fa6bed00d
003 <apple@003-MacBook-Air.local>
2024-08-02 12:48:19 +0800

    App.js

--- App.js
+++ App.js
@@ -20,8 +20,8 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    // origin: process.env.NETLIFY_URL || "http://localhost:3000",
-    origin: "http://localhost:3000",
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    // origin: "http://localhost:3000",
   })
 );
 
